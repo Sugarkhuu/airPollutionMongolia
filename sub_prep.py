93ad1068795ef5e06d67f379bad99ad1682a9bc2
sugarkhuu <sugarkhuu.radnaa@ogresearch.com>
2020-12-07 01:26:36 +0800

    60.4, removed 75% of deviation from history for those which are deviating more than 25

--- sub_prep.py
+++ sub_prep.py
@@ -8,14 +8,28 @@ my_sub = linear.copy()
 my_sub['aqi'] = lin_weight*linear['aqi'] + cat_weight*cat['aqi']
 
 
+tmp = pm_train.groupby(['station','month','hour'])['aqi'].mean()
+tmp=tmp.reset_index()
+tmp = tmp.rename(columns={'aqi': "aqi_mean"})
+#del pm_test['aqi_mean_y']
+#pm_test = pm_test.rename(columns={'aqi_mean_x': "aqi_mean"})
+pm_test = pm_test.merge(tmp,on=['station','month','hour'],how='left')
+
+
 check = my_sub.copy()
 check['aqi_mean_diff'] = my_sub['aqi'] - pm_test['aqi_mean']
 check['aqi_mean_diff'].hist(bins=100)
 
 
 
+corr_factor = .75
+corr_thres  =  25
 #my_sub.loc[check['aqi_mean_diff']<-100,'aqi'] = my_sub.loc[check['aqi_mean_diff']<-100,'aqi'] + 15
-my_sub.loc[check['aqi_mean_diff']>50,'aqi'] = my_sub.loc[check['aqi_mean_diff']>50,'aqi'] - 25
+my_sub.loc[check['aqi_mean_diff']>corr_thres,'aqi'] = my_sub.loc[check['aqi_mean_diff']>corr_thres,'aqi'] - corr_factor*check.loc[check['aqi_mean_diff']>corr_thres,'aqi_mean_diff']
+
+# readjust for those already given
+my_sub.loc[~pm_test['error'].isnull(),'aqi'] = linear.loc[~pm_test['error'].isnull(),'aqi']
+
 
 
 submission = my_sub[['ID','aqi']].copy()
@@ -25,7 +39,7 @@ submission.to_csv('submission.csv',index=False)
 
 ###############################################################################
 
-subB = pd.read_csv('sub605.csv')
+subB = pd.read_csv('sub602.csv')
 print(np.sqrt(mean_squared_error(subB['aqi'],my_sub['aqi'])))
 
 
